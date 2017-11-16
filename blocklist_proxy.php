6ddbbc4e97e2068f17b0026097dc550020387a3b
Clemens Jung <clemens.michael.jung@gmail.com>
2017-11-16 22:13:55 +0100

    minor improvement

--- blocklist_proxy.php
+++ blocklist_proxy.php
@@ -4,7 +4,7 @@ echo "[Adblock Plus 2.0]\r\n";
 
 $homepage = file_get_contents($_GET['url']);
 $homepage = preg_replace('/\r\n|\r|\n/', "\r\n", $homepage );
-$homepage = preg_replace('/\[Adblock\]/', "", $homepage );
+$homepage = preg_replace('/\[Adblock.*\]/', "", $homepage );
 
 echo preg_replace('/[#;!]/i','!',$homepage);
 ?>
